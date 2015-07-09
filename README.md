9196930bd0f9a872552775d95eeb9b107d49cc0b
Turro75 <valerio.turrini@gmail.com>
2015-07-09 16:05:52 +0200

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# commandlineecho
+
+cisos
---
+++ unit1.pas
@@ -0,0 +1,46 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Label1: TLabel;
+    procedure Button1Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  label1.caption:=Application.GetOptionValue('f','file');
+  label1.caption:=Application.GetOptionValue('f','file');
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+
+end;
+
+end.
+
